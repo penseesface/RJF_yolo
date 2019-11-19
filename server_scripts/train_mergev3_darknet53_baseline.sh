6f43f5bfc228d4f72c2e5163304fdc64bc115194
Server 249 <2355396t@student.gla.ac.uk>
2019-11-19 14:17:08 +0800

    Sync changes to other server

--- server_scripts/train_mergev3_darknet53_baseline.sh
+++ server_scripts/train_mergev3_darknet53_baseline.sh
@@ -4,7 +4,9 @@ python train.py \
 --exp_id mergev3_darknet53_baseline \
 --epochs 200 \
 --accumulate 1 \
---batch-size 80 \
+--batch-size 140 \
 --data data/merge.data \
 --weights exp/mergev3_darknet53_baseline/model_last.pt \
---cfg cfg/yolov3-custom.cfg
+--cfg cfg/yolov3-custom.cfg \
+--test_interval 5 \
+--evolve
