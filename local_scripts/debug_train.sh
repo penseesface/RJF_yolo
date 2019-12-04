6ab2bc839be004391c6dda23dad2bf0bfe79cc66
Jeff <2355396t@student.gla.ac.uk>
2019-12-04 15:37:59 +0800

    Added new anchor size for training in 640 resolution

--- local_scripts/debug_train.sh
+++ local_scripts/debug_train.sh
@@ -2,8 +2,9 @@ cd ..
 
 python train.py \
 --exp_id debug \
---data data/merge.data \
+--data data/wuxiv2.data \
 --batch-size 16 \
 --accumulate 1 \
 --weights weights/yolov3.weights \
---cfg cfg/yolov3-custom.cfg
+--cfg cfg/yolov3-custom_v2.cfg \
+--img-size 640
